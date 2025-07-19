6477a4dc886af173cbf627b3598464819272957f
Hayden Dingman <haydendingman@gmail.com>
2025-07-18 20:45:00 -0700

    Refactor to use form action instead of onsubmit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,22 +2,20 @@ import {useState} from "react"
 
 function Main() {
 
-    const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+    const [ingredients, setIngredients] = useState([])
     
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
+    function handleSubmit(formData) {
         const newIngredient = formData.get("ingredient");
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
     return(
         <main>
-            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+            <form className="add-ingredient-form" action={handleSubmit}>
                 <input type="text" aria-label="Add ingredient" placeholder="e.g. oregano" className="field-add-ingredient" name="ingredient" />
                 <button className="btn-add-ingredient">+ Add Ingredient</button>
             </form>
