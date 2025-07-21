f7af859b507fb5b0085360dcaa0bd3596a03e970
Hayden Dingman <haydendingman@gmail.com>
2025-07-21 14:30:20 -0700

    Make recipe prompt only show after four ingredients are added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,13 +23,13 @@ function Main() {
             <section className="width-container">
                 <h2>Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-                <div className="get-recipe-container">
+                {ingredients.length > 3 ? <div className="get-recipe-container">
                     <div>
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
-                </div>
+                </div> : null}
             </section> : null}
         </main>
     )
